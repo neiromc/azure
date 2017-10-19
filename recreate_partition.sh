54950bca1a1c2860619b0cc0768133736f9656f4
Yuri Boykov <y.boykov@3adigital.ru>
2017-10-19 13:58:38 +0300

    add systemctl target

--- recreate_partition.sh
+++ recreate_partition.sh
@@ -4,7 +4,11 @@
 # by Neiro.
 
 
-LOCKFILE=/azure-script.lock
+SERVICE_NAME=azure-deploy
+SERVICE_PATH=/etc/systemd/system/
+
+LOCKFILE=/${SERVICE_NAME}.lock
+
 
 DISK=$1
 
@@ -21,8 +25,27 @@ w"| fdisk "$DISK" > /dev/null 2>&1
 
 touch $LOCKFILE
 
+echo "
+[Unit]
+Description=3adigital Resize XFS
+After=network-online.target
+
+[Service]
+Type=oneshot
+ExecStart=/var/lib/waagent/custom-script/download/0/recreate_partition.sh
+RemainAfterExit=yes
+User=root
+Group=root
+
+[Install]
+WantedBy=multi-user.target
+" > ${SERVICE_PATH}/${SERVICE_NAME}.service
+
+systemctl daemon-reload
+systemctl enable $SERVICE_NAME
+
 else
  echo "Resize disk..."
- xfs_growfs /dev/sda2 && rm $LOCKFILE
+ xfs_growfs /dev/sda2 && systemctl disable $SERVICE_NAME && rm $LOCKFILE ${SERVICE_PATH}/${SERVICE_NAME}.service && systemctl daemon-reload
 fi
 
